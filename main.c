47479825f2b00122fa84113afa3f139c07e9ab2b
HendrikVE <hendrik1995@msn.com>
2019-03-23 14:00:39 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,286 @@
+/*
+ * Copyright (C) 2018 Freie Universität Berlin
+ *               2018 Codecoup
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       BLE peripheral example using NimBLE
+ *
+ * Have a more detailed look at the api here:
+ * https://mynewt.apache.org/latest/tutorials/ble/bleprph/bleprph.html
+ *
+ * More examples (not ready to run on RIOT) can be found here:
+ * https://github.com/apache/mynewt-nimble/tree/master/apps
+ *
+ * Test this application e.g. with Nordics "nRF Connect"-App
+ * iOS: https://itunes.apple.com/us/app/nrf-connect/id1054362403
+ * Android: https://play.google.com/store/apps/details?id=no.nordicsemi.android.mcp
+ *
+ * @author      Hauke Petersen <hauke.petersen@fu-berlin.de>
+ * @author      Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
+ * @author      Hendrik van Essen <hendrik.ve@fu-berlin.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "nimble_riot.h"
+#include "net/bluetil/ad.h"
+
+#include "host/ble_hs.h"
+#include "host/util/util.h"
+#include "host/ble_gatt.h"
+#include "services/gap/ble_svc_gap.h"
+#include "services/gatt/ble_svc_gatt.h"
+
+#define GATT_DEVICE_INFO_UUID                   0x180A
+#define GATT_MANUFACTURER_NAME_UUID             0x2A29
+#define GATT_MODEL_NUMBER_UUID                  0x2A24
+
+#define STR_ANSWER_BUFFER_SIZE 100
+
+/* UUID = e6d54866-0292-4779-b8f8-c52bbec91e71 */
+static const ble_uuid128_t gatt_svr_svc_stdio_uuid
+        = BLE_UUID128_INIT(0x71, 0x1e, 0xc9, 0xbe, 0x2b, 0xc5, 0xf8, 0xb8, 0x79,
+                           0x47, 0x92, 0x02, 0x66, 0x48, 0xd5, 0xe6);
+
+/* UUID = 35f28386-3070-4f3b-ba38-27507e991762 */
+static const ble_uuid128_t gatt_svr_chr_stdout_uuid
+        = BLE_UUID128_INIT(0x62, 0x17, 0x99, 0x7e, 0x50, 0x27, 0x38, 0xba, 0x3b,
+                0x4f, 0x70, 0x30, 0x86, 0x83, 0xf2, 0x35);
+
+/* UUID = ccdd113f-40d5-4d68-86ac-a728dd82f4aa */
+static const ble_uuid128_t gatt_svr_chr_stdin_uuid
+        = BLE_UUID128_INIT(0xaa, 0xf4, 0x82, 0xdd, 0x28, 0xa7, 0xac, 0x86, 0x68,
+                0x4d, 0xd5, 0x40, 0x3f, 0x11, 0xdd, 0xcc);
+
+static const char *device_name = "Lord NimBLEer";
+
+static int gatt_svr_chr_access_device_info_manufacturer(
+        uint16_t conn_handle, uint16_t attr_handle,
+        struct ble_gatt_access_ctxt *ctxt, void *arg);
+
+static int gatt_svr_chr_access_device_info_model(
+        uint16_t conn_handle, uint16_t attr_handle,
+        struct ble_gatt_access_ctxt *ctxt, void *arg);
+
+static int gatt_svr_chr_access_stdout(
+        uint16_t conn_handle, uint16_t attr_handle,
+        struct ble_gatt_access_ctxt *ctxt, void *arg);
+
+static int gatt_svr_chr_access_stdin(
+        uint16_t conn_handle, uint16_t attr_handle,
+        struct ble_gatt_access_ctxt *ctxt, void *arg);
+
+static void start_advertise(void);
+
+static char str_answer[STR_ANSWER_BUFFER_SIZE];
+
+static char str_previous_cmd[STR_ANSWER_BUFFER_SIZE];
+
+/* define several bluetooth services for our device */
+static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
+    /*
+     * access_cb defines a callback for read and write access events on
+     * given characteristics
+     */
+    {
+        /* Service: Device Information */
+        .type = BLE_GATT_SVC_TYPE_PRIMARY,
+        .uuid = BLE_UUID16_DECLARE(GATT_DEVICE_INFO_UUID),
+        .characteristics = (struct ble_gatt_chr_def[]) { {
+            /* Characteristic: * Manufacturer name */
+            .uuid = BLE_UUID16_DECLARE(GATT_MANUFACTURER_NAME_UUID),
+            .access_cb = gatt_svr_chr_access_device_info_manufacturer,
+            .flags = BLE_GATT_CHR_F_READ,
+        }, {
+            /* Characteristic: Model number string */
+            .uuid = BLE_UUID16_DECLARE(GATT_MODEL_NUMBER_UUID),
+            .access_cb = gatt_svr_chr_access_device_info_model,
+            .flags = BLE_GATT_CHR_F_READ,
+        }, {
+            0, /* No more characteristics in this service */
+        }, }
+    },
+    {
+        /* Service: stdio */
+        .type = BLE_GATT_SVC_TYPE_PRIMARY,
+        .uuid = (ble_uuid_t*) &gatt_svr_svc_stdio_uuid.u,
+        .characteristics = (struct ble_gatt_chr_def[]) { {
+            /* Characteristic: stdout */
+            .uuid = (ble_uuid_t*) &gatt_svr_chr_stdout_uuid.u,
+            .access_cb = gatt_svr_chr_access_stdout,
+            .flags = BLE_GATT_CHR_F_WRITE,
+        }, {
+            /* Characteristic: stdin */
+            .uuid = (ble_uuid_t*) &gatt_svr_chr_stdin_uuid.u,
+            .access_cb = gatt_svr_chr_access_stdin,
+            .flags = BLE_GATT_CHR_F_READ | BLE_GATT_CHR_F_NOTIFY,
+        }, {
+            0, /* No more characteristics in this service */
+        }, }
+    },
+    {
+        0, /* No more services */
+    },
+};
+
+static int gap_event_cb(struct ble_gap_event *event, void *arg)
+{
+    (void)arg;
+
+    switch (event->type) {
+        case BLE_GAP_EVENT_CONNECT:
+            if (event->connect.status) {
+                start_advertise();
+            }
+            break;
+
+        case BLE_GAP_EVENT_DISCONNECT:
+            start_advertise();
+            break;
+    }
+
+    return 0;
+}
+
+static void start_advertise(void)
+{
+    struct ble_gap_adv_params advp;
+    int rc;
+
+    memset(&advp, 0, sizeof advp);
+    advp.conn_mode = BLE_GAP_CONN_MODE_UND;
+    advp.disc_mode = BLE_GAP_DISC_MODE_GEN;
+    rc = ble_gap_adv_start(nimble_riot_own_addr_type, NULL, BLE_HS_FOREVER,
+                           &advp, gap_event_cb, NULL);
+    assert(rc == 0);
+    (void)rc;
+}
+
+static int gatt_svr_chr_access_device_info_manufacturer(
+        uint16_t conn_handle, uint16_t attr_handle,
+        struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    puts("service 'device info: manufacturer' callback triggered");
+
+    (void) conn_handle;
+    (void) attr_handle;
+    (void) arg;
+
+    snprintf(str_answer, STR_ANSWER_BUFFER_SIZE,
+             "This is RIOT! (Version: %s)\n", RIOT_VERSION);
+    puts(str_answer);
+
+    int rc = os_mbuf_append(ctxt->om, str_answer, strlen(str_answer));
+
+    puts("");
+
+    return rc;
+}
+
+static int gatt_svr_chr_access_device_info_model(
+        uint16_t conn_handle, uint16_t attr_handle,
+        struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    puts("service 'device info: model' callback triggered");
+
+    (void) conn_handle;
+    (void) attr_handle;
+    (void) arg;
+
+    snprintf(str_answer, STR_ANSWER_BUFFER_SIZE,
+             "You are running RIOT on a(n) %s board, "
+             "which features a(n) %s MCU.", RIOT_BOARD, RIOT_MCU);
+    puts(str_answer);
+
+    int rc = os_mbuf_append(ctxt->om, str_answer, strlen(str_answer));
+
+    puts("");
+
+    return rc;
+}
+
+static int gatt_svr_chr_access_stdout(
+        uint16_t conn_handle, uint16_t attr_handle,
+        struct ble_gatt_access_ctxt *ctxt, void *arg) {
+
+    puts("gatt_svr_chr_access_stdout");
+
+    (void) conn_handle;
+    (void) attr_handle;
+    (void) arg;
+    (void) ctxt;
+
+    uint16_t om_len;
+    om_len = OS_MBUF_PKTLEN(ctxt->om);
+
+    /* read sent data */
+    int rc = ble_hs_mbuf_to_flat(ctxt->om, &str_previous_cmd,
+                             sizeof str_previous_cmd, &om_len);
+    /* we need to null-terminate the received string */
+    str_previous_cmd[om_len] = '\0';
+
+    printf("new command to be processed: '%s'\n",
+           str_previous_cmd);
+
+    return rc;
+}
+
+static int gatt_svr_chr_access_stdin(
+        uint16_t conn_handle, uint16_t attr_handle,
+        struct ble_gatt_access_ctxt *ctxt, void *arg) {
+
+    puts("gatt_svr_chr_access_stdin");
+
+    (void) conn_handle;
+    (void) attr_handle;
+    (void) arg;
+
+    snprintf(str_answer, STR_ANSWER_BUFFER_SIZE, "this is the output for command: '%s'", str_previous_cmd);
+
+    int rc = os_mbuf_append(ctxt->om, str_answer, strlen(str_answer));
+
+    return rc;
+}
+
+int main(void)
+{
+    puts("Shell over NimBLE GATT Server Example");
+
+    int rc = 0;
+
+    /* verify and add our custom services */
+    rc = ble_gatts_count_cfg(gatt_svr_svcs);
+    assert(rc == 0);
+    rc = ble_gatts_add_svcs(gatt_svr_svcs);
+    assert(rc == 0);
+
+    /* set the device name */
+    ble_svc_gap_device_name_set(device_name);
+    /* reload the GATT server to link our added services */
+    ble_gatts_start();
+
+    /* configure and set the advertising data */
+    uint8_t buf[BLE_HS_ADV_MAX_SZ];
+    bluetil_ad_t ad;
+    bluetil_ad_init_with_flags(&ad, buf, sizeof(buf), BLUETIL_AD_FLAGS_DEFAULT);
+    bluetil_ad_add_name(&ad, device_name);
+    ble_gap_adv_set_data(ad.buf, ad.pos);
+
+    /* start to advertise this node */
+    start_advertise();
+
+    return 0;
+}
